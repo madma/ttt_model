5137542251ed57a7ca20e95544b4b4036e1c4577
Michael Duran <mad2116@gmail.com>
2016-01-26 13:00:01 -0800

    finish model; add view

--- main.js
+++ main.js
@@ -13,6 +13,23 @@ var board = [
 ];
 
 // 1. START THE GAME
+/*    inputs:
+        -currentPlayer, board, won
+
+      outputs:
+        -board is emptied, player is X, won is false
+*/
+
+var startGame = function() {
+  won = false;
+  currentPlayer = "X";
+  board = [
+    "", "", "",   // 0, 1, 2
+    "", "", "",   // 3, 4, 5
+    "", "", "",   // 6, 7, 8
+  ];
+};
+
 // 2. MOVE (CHANGES TURN)
 /*    inputs:
         -whose turn it is,
@@ -38,7 +55,31 @@ var move = function (cellIndex) {
 };
 
 // 3. WIN THE GAME
-var gameWon = function() {}; // predicate function -- returns true or false
+/*    inputs:
+        -board
 
+      outputs:
+        - true or false
+*/
+var gameWon = function() {
+  if (
+    // alternatively, currentPlayer = 1/0/-1 with condition Math.abs(board[0] + board[1] + board [2] === 3)
+    ((board[0] === board[1]) && (board[0] === board[2]) && board[0] != "") ||
+    ((board[3] === board[4]) && (board[3] === board[5]) && board[3] != "") ||
+    ((board[6] === board[7]) && (board[6] === board[8]) && board[6] != "")
+    ) {
+    return true;
+  } else {
+    return false;
+  }
+}; // predicate function -- returns true or false
+
+
+//  RENDER OUR VIEW
+
+//whenever called, render will draw the state of the model, only one function to update the view
+var render = function() {
+
+};
 
 // });
